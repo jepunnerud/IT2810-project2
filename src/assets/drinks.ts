8419ba16f1cc1383002f9a27ad1bf69679a14ea2
Jakob Eilertsen Punnerud <jakobep@stud.ntnu.no>
2023-10-09 22:27:47 +0200

    GL-3: Fix data format, switch to ts file, adjust types

---
+++ src/assets/drinks.ts
@@ -0,0 +1,280 @@
+export const drinks = [
+  {
+    drinkid: '15997',
+    name: 'GG',
+    category: 'Ordinary Drink',
+    alcoholic: true,
+    glass: 'Collins Glass',
+    instructions:
+      'Pour the Galliano liqueur over ice. Fill the remainder of the glass with ginger ale and thats all there is to it. You now have a your very own GG.',
+    strInstructionsES: null,
+    strInstructionsDE:
+      'Den Galliano-Likör über Eis gießen. Füllen Sie den Rest des Glases mit Ginger Ale und das ist alles, was dazu gehört. Du hast jetzt ein eigenes GG.',
+    strInstructionsFR: null,
+    strInstructionsIT:
+      'Versare il liquore Galliano su ghiaccio.\r\nRiempi il resto del bicchiere con ginger ale e questo è tutto.\r\nOra hai il tuo GG personale.',
+    'strInstructionsZH-HANS': null,
+    'strInstructionsZH-HANT': null,
+    picture:
+      'https://www.thecocktaildb.com/images/media/drink/vyxwut1468875960.jpg',
+    ingredients: [
+      {
+        ingredient: 'Galliano',
+        measure: '2 1/2 shots ',
+      },
+      {
+        ingredient: 'Ginger ale',
+      },
+      {
+        ingredient: 'Ice',
+      },
+    ],
+  },
+  {
+    drinkid: '17222',
+    name: 'A1',
+    category: 'Cocktail',
+    alcoholic: true,
+    glass: 'Cocktail glass',
+    instructions:
+      'Pour all ingredients into a cocktail shaker, mix and serve over ice into a chilled glass.',
+    strInstructionsES:
+      'Vierta todos los ingredientes en una coctelera, mezcle y sirva con hielo en un vaso frío.',
+    strInstructionsDE:
+      'Alle Zutaten in einen Cocktailshaker geben, mischen und über Eis in ein gekühltes Glas servieren.',
+    strInstructionsFR: null,
+    strInstructionsIT:
+      'Versare tutti gli ingredienti in uno shaker, mescolare e servire con ghiaccio in un bicchiere freddo.',
+    'strInstructionsZH-HANS': null,
+    'strInstructionsZH-HANT': null,
+    picture:
+      'https://www.thecocktaildb.com/images/media/drink/2x8thr1504816928.jpg',
+    ingredients: [
+      { ingredient: 'Gin', measure: '1 3/4 shot ' },
+      { ingredient: 'Grand Marnier', measure: '1 Shot ' },
+      { ingredient: 'Lemon Juice', measure: '1/4 Shot' },
+      { ingredient: 'Grenadine', measure: '1/8 Shot' },
+    ],
+  },
+  {
+    drinkid: '17225',
+    name: 'Ace',
+    category: 'Cocktail',
+    alcoholic: true,
+    glass: 'Martini Glass',
+    instructions:
+      'Shake all the ingredients in a cocktail shaker and ice then strain in a cold glass.',
+    strInstructionsES:
+      'Agite todos los ingredientes en una coctelera con hielo y entonces cuélelos sobre un vaso enfriado.',
+    strInstructionsDE:
+      'Alle Zutaten in einem Cocktailshaker mit Eis schütteln und dann in einem kalten Glas abseihen.',
+    strInstructionsFR: null,
+    strInstructionsIT:
+      'Shakerare tutti gli ingredienti in uno shaker e ghiaccio, quindi filtrare in un bicchiere freddo.',
+    'strInstructionsZH-HANS': null,
+    'strInstructionsZH-HANT': null,
+    picture:
+      'https://www.thecocktaildb.com/images/media/drink/l3cd7f1504818306.jpg',
+    ingredients: [
+      { ingredient: 'Gin', measure: '2 shots ' },
+      { ingredient: 'Grenadine', measure: '1/2 shot ' },
+      { ingredient: 'Heavy cream', measure: '1/2 shot ' },
+      { ingredient: 'Milk', measure: '1/2 shot' },
+      { ingredient: 'Egg White', measure: '1/2 Fresh' },
+    ],
+  },
+  {
+    drinkid: '14229',
+    name: '747',
+    category: 'Shot',
+    alcoholic: true,
+    glass: 'Shot glass',
+    instructions:
+      'pour kaluha, then Baileys, then Frangelico not chilled and not layered -- SERVE!!!',
+    strInstructionsES: null,
+    strInstructionsDE:
+      'Gießen Sie kaluha, dann Baileys, dann Frangelico nicht gekühlt und nicht geschichtet -- SERVE!!!!!!!!',
+    strInstructionsFR: null,
+    strInstructionsIT: 'Versa Kahlua, Baileys e poi Frangelico.',
+    'strInstructionsZH-HANS': null,
+    'strInstructionsZH-HANT': null,
+    picture:
+      'https://www.thecocktaildb.com/images/media/drink/xxsxqy1472668106.jpg',
+    ingredients: [
+      { ingredient: 'Kahlua', measure: '1/3 part ' },
+      { ingredient: 'Baileys irish cream', measure: '1/3 part ' },
+      { ingredient: 'Frangelico', measure: '1/3 part ' },
+    ],
+  },
+  {
+    drinkid: '17203',
+    name: 'Kir',
+    category: 'Ordinary Drink',
+    alcoholic: true,
+    glass: 'Wine Glass',
+    instructions:
+      'Add the crème de cassis to the bottom of the glass, then top up with wine.',
+    strInstructionsES: null,
+    strInstructionsDE:
+      'Gib die Creme de Cassis auf den Boden des Glases und füge dann Wein hinzu.',
+    strInstructionsFR: null,
+    strInstructionsIT:
+      'Aggiungere la crème de cassis sul fondo del bicchiere, quindi rabboccare con il vino.',
+    'strInstructionsZH-HANS': null,
+    'strInstructionsZH-HANT': null,
+    picture:
+      'https://www.thecocktaildb.com/images/media/drink/apneom1504370294.jpg',
+    ingredients: [
+      { ingredient: 'Creme de Cassis', measure: '1 part ' },
+      { ingredient: 'Champagne', measure: '5 parts ' },
+    ],
+  },
+  {
+    drinkid: '13501',
+    name: 'ABC',
+    category: 'Shot',
+    alcoholic: true,
+    glass: 'Shot glass',
+    instructions: 'Layered in a shot glass.',
+    strInstructionsES: 'Coloque todos los ingredientes en un vaso de chupito.',
+    strInstructionsDE: 'Schichtaufbau in einem Schnapsglas.',
+    strInstructionsFR: null,
+    strInstructionsIT:
+      "Versa in ordine di lettera i vari ingredienti. 1/3 del bicchiere va riempito con l'Amaretto, 1/3 di Baileys e il restante di Cognac.",
+    'strInstructionsZH-HANS': null,
+    'strInstructionsZH-HANT': null,
+    picture:
+      'https://www.thecocktaildb.com/images/media/drink/tqpvqp1472668328.jpg',
+    ingredients: [
+      { ingredient: 'Amaretto', measure: '1/3 ' },
+      { ingredient: 'Baileys irish cream', measure: '1/3 ' },
+      { ingredient: 'Cognac', measure: '1/3 ' },
+    ],
+  },
+  {
+    drinkid: '15288',
+    name: '252',
+    category: 'Shot',
+    alcoholic: true,
+    glass: 'Shot glass',
+    instructions:
+      'Add both ingredients to shot glass, shoot, and get drunk quick',
+    strInstructionsES: null,
+    strInstructionsDE: 'Beide Zutaten in ein Schnapsglas geben.',
+    strInstructionsFR: null,
+    strInstructionsIT:
+      'Aggiungi entrambi gli ingredienti al bicchierino, bevi e ubriacati velocemente',
+    'strInstructionsZH-HANS': null,
+    'strInstructionsZH-HANT': null,
+    picture:
+      'https://www.thecocktaildb.com/images/media/drink/rtpxqw1468877562.jpg',
+    ingredients: [
+      { ingredient: '151 proof rum', measure: '1/2 shot Bacardi ' },
+      { ingredient: 'Wild Turkey', measure: '1/2 shot ' },
+    ],
+  },
+  {
+    drinkid: '13938',
+    name: 'AT&T',
+    category: 'Ordinary Drink',
+    alcoholic: true,
+    glass: 'Highball Glass',
+    instructions: 'Pour Vodka and Gin over ice, add Tonic and Stir',
+    strInstructionsES:
+      'Vierta el vodka y la ginebra sobre el hielo, agregue la tónica y revuelva.',
+    strInstructionsDE:
+      'Wodka und Gin über das Eis gießen, Tonic hinzufügen und umrühren.',
+    strInstructionsFR: null,
+    strInstructionsIT:
+      "Versare la vodka e il gin sul ghiaccio, aggiungere l'acqua tonica e mescolare",
+    'strInstructionsZH-HANS': null,
+    'strInstructionsZH-HANT': null,
+    picture:
+      'https://www.thecocktaildb.com/images/media/drink/rhhwmp1493067619.jpg',
+    ingredients: [
+      { ingredient: 'Absolut Vodka', measure: '1 oz ' },
+      { ingredient: 'Gin', measure: '1 oz ' },
+      { ingredient: 'Tonic water', measure: '4 oz ' },
+    ],
+  },
+  {
+    drinkid: '17141',
+    name: 'Smut',
+    category: 'Punch / Party Drink',
+    alcoholic: true,
+    glass: 'Beer mug',
+    instructions: 'Throw it all together and serve real cold.',
+    strInstructionsES: null,
+    strInstructionsDE: 'Schütte alles zusammen und serviere es kalt.',
+    strInstructionsFR: null,
+    strInstructionsIT: 'Metti tutto insieme e servi freddo.',
+    'strInstructionsZH-HANS': null,
+    'strInstructionsZH-HANT': null,
+    picture:
+      'https://www.thecocktaildb.com/images/media/drink/rx8k8e1504365812.jpg',
+    ingredients: [
+      { ingredient: 'Red wine', measure: '1/3 part' },
+      { ingredient: 'Peach schnapps', measure: '1 shot' },
+      { ingredient: 'Red wine', measure: '1/3 part' },
+      { ingredient: 'Orange juice', measure: '1/3 part' },
+    ],
+  },
+  {
+    drinkid: '13332',
+    name: 'B-53',
+    category: 'Shot',
+    alcoholic: true,
+    glass: 'Collins Glass',
+    instructions:
+      'Layer the Kahlua, Sambucca and Grand Marnier into a shot glas in that order. Better than B-52',
+    strInstructionsES: null,
+    strInstructionsDE:
+      'Schichte die Kahlua, Sambucca und Grand Marnier in dieser Reihenfolge in ein Schnapsglas ein. Besser als B-52',
+    strInstructionsFR: null,
+    strInstructionsIT:
+      "Metti Kahlua, Sambuca e Grand Marnier in un bicchierino in quest'ordine. ",
+    'strInstructionsZH-HANS': null,
+    'strInstructionsZH-HANT': null,
+    picture:
+      'https://www.thecocktaildb.com/images/media/drink/rwqxrv1461866023.jpg',
+    ingredients: [
+      {
+        ingredient: 'Kahlua',
+        measure: '1/3 shot ',
+      },
+      {
+        ingredient: 'Sambuca',
+        measure: '1/3 shot ',
+      },
+      {
+        ingredient: 'Grand Marnier',
+        measure: '1/3 shot ',
+      },
+    ],
+  },
+  {
+    drinkid: '17837',
+    name: 'Adam',
+    category: 'Ordinary Drink',
+    alcoholic: true,
+    glass: 'Cocktail glass',
+    instructions:
+      'In a shaker half-filled with ice cubes, combine all of the ingredients. Shake well. Strain into a cocktail glass.',
+    strInstructionsES:
+      'Mezclar todos los ingredientes en una coctelera con cubitos de hielo hasta la mitad. Agitar bien. Colar el contenido en un vaso de cóctel.',
+    strInstructionsDE:
+      'In einem Shaker, der halb mit Eiswürfeln gefüllt ist, alle Zutaten vermengen. Gut schütteln. In ein Cocktailglas abseihen.',
+    strInstructionsFR: null,
+    strInstructionsIT:
+      'In uno shaker riempito a metà con cubetti di ghiaccio, unire tutti gli ingredienti.Agitare bene.Filtrare in un bicchiere da cocktail.',
+    'strInstructionsZH-HANS': null,
+    'strInstructionsZH-HANT': null,
+    picture:
+      'https://www.thecocktaildb.com/images/media/drink/v0at4i1582478473.jpg',
+    ingredients: [
+      { ingredient: 'Dark rum', measure: '2 oz ' },
+      { ingredient: 'Lemon juice', measure: '1 oz ' },
+      { ingredient: 'Grenadine', measure: '1 tsp ' },
+    ],
+  },
+]
