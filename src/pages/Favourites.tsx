b044b9e30fdf656e4d05f95375ae2fdd9af038f2
Jakob Eilertsen Punnerud <jakobep@stud.ntnu.no>
2023-10-11 23:38:39 +0200

    GL-17: Center title on favourites page

--- src/pages/Favourites.tsx
+++ src/pages/Favourites.tsx
@@ -3,6 +3,7 @@ import DrinkCard from '../components/DrinkCard'
 import { useDrinks } from '../hooks/Drinks'
 import { sortingFns } from '../utils/constants'
 import '../utils/Loader.css'
+import './Favourites.css'
 
 export default function FavouritesPage() {
   const storedFavourites = localStorage.getItem('favourites')
@@ -12,13 +13,13 @@ export default function FavouritesPage() {
     : []
   if (isLoading) return <span className="loader"></span>
   return (
-    <>
+    <div className="favourite-page-container">
       <h1>Favoritter</h1>
       <div className="card-container">
         {favoriteDrinks
           ?.sort(sortingFns['alphabetically'])
           .map((d) => <DrinkCard drink={d} />)}
       </div>
-    </>
+    </div>
   )
 }
