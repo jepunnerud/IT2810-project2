9c617c15bf9fa1999693b30c7da1ddb72f589b97
Andreas Omholt Olsen <andreaoo@stud.ntnu.no>
2023-11-07 13:06:42 +0100

    GL-19: Add MongoDB Atlas cluster

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,12 @@
-import express, { Express, Request, Response } from "express";
-import mongoose from "mongoose";
-import typeDefs from "./graphql/typeDefs"; // Assuming this is where your GraphQL type definitions are
-import resolvers from "./graphql/resolvers"; // 
-import { ApolloServer } from "apollo-server-express";
+import express, { Express, Request, Response } from 'express'
+import mongoose from 'mongoose'
+import typeDefs from './graphql/typeDefs' // Assuming this is where your GraphQL type definitions are
+import resolvers from './graphql/resolvers' //
+import { ApolloServer } from 'apollo-server-express'
+import 'dotenv/config'
 
 async function startServer() {
-
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 3000
   //const PORT = 8000
 
   const app = express()
@@ -17,32 +17,35 @@ async function startServer() {
   })
 
   await server.start()
-  server.applyMiddleware({ app: app, path: "/graphql" });
+  server.applyMiddleware({ app: app, path: '/graphql' })
 
   app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    if (req.method == "OPTIONS") {
-      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    res.header('Access-Control-Allow-Origin', '*')
+    if (req.method == 'OPTIONS') {
+      res.header(
+        'Access-Control-Allow-Methods',
+        'PUT, POST, PATCH, DELETE, GET'
+      )
     }
-    next();
-  });
+    next()
+  })
 
-  mongoose.set("strictQuery", true);
+  mongoose.set('strictQuery', true)
   mongoose
-    .connect("mongodb://127.0.0.1:27017/cocktail-connoisseur-api", {
-      dbName: "connoisseurDB",
-      retryWrites: true,
-      w: "majority",
-    })
-    .then(() => console.log("connected"))
-    .catch((e) => console.log(e));
-
+    .connect(
+      `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cocktailconnoisseur.u1rupiz.mongodb.net/?retryWrites=true&w=majority`,
+      {
+        dbName: 'connoisseurDB',
+        retryWrites: true,
+        w: 'majority',
+      }
+    )
+    .then(() => console.log('connected'))
+    .catch((e) => console.log(e))
 
   app.listen(PORT, () => {
-    console.log(`Apollo Server on http://localhost:${PORT}/graphql`);
-  });
-
+    console.log(`Apollo Server on http://localhost:${PORT}/graphql`)
+  })
 }
 
-startServer();
-
+startServer()
