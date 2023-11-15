69e474c71afe8d3069e790419538ed4c92a40c75
Jakob Eilertsen Punnerud <jakobep@stud.ntnu.no>
2023-11-15 14:19:18 +0100

    GL-35: Make theme button keyboard focusable and clickable with enter

--- client/src/components/ToggleThemeButton.tsx
+++ client/src/components/ToggleThemeButton.tsx
@@ -4,8 +4,14 @@ const ToggleThemeButton = () => {
   const theme = useTheme()
   const toggleTheme = useUpdateTheme()
 
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLSpanElement>) => {
+    if (e.key === 'Enter') {
+      toggleTheme()
+    }
+  }
+
   return (
-    <div>
+    <div tabIndex={0} onKeyDown={handleKeyDown}>
       <link
         rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
