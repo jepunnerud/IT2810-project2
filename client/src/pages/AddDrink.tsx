0f14cd4e487e16467994bd28abea1dda3174128e
larshfle <larshfle@stud.ntnu.no>
2023-11-18 15:17:57 +0100

    fixed prettier

--- client/src/pages/AddDrink.tsx
+++ client/src/pages/AddDrink.tsx
@@ -9,126 +9,131 @@ function AddDrink() {
     control,
     name: 'ingredients',
   })
-  
 
   function onSubmit(data: object) {
     console.log('data', data)
   }
 
   return (
-  <div className={`add-drink-container ${theme}`}>
+    <div className={`add-drink-container ${theme}`}>
       <h1>✨Add new drinks here✨</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className={`add-drink-card ${theme}`}>
-          
-            <h3>Name: <input
-                      {...register('name', {
-                        required: 'Your drink needs a name',
-                      })}
-                    ></input>
-            </h3>
-            <h3>Ingredients: 
-              <section>
-                <button
-                  className={`button ${theme}`}
-                  type="button"
-                  onClick={() => {
-                    append({ ingredient: '', measure: '' })
-                  }}
-                >
-                  Add new ingredient
-                </button>
-              </section>
-            </h3>
-            <ul>
-              {fields.map((item, index) => {
-                return (
-                  <p key={item.id}>
-                    <input
-                      {...register(`ingredients.${index}.ingredient`, {
-                        required: true,
-                      })}
-                      placeholder="ingredient"
-                    />
-                    <Controller
-                      render={({ field }) => <input {...field} placeholder="measure" />}
-                      name={`ingredients.${index}.measure`}
-                      control={control}
-                    />
-                    <button className={`button ${theme}`} type="button" onClick={() => remove(index)}>
-                      Delete ingredient
-                    </button>
-                  </p>
-                )
+          <h3>
+            Name:{' '}
+            <input
+              {...register('name', {
+                required: 'Your drink needs a name',
               })}
-            </ul>
-            <h3>Instructions: </h3>
-            <textarea
-              className="instructions-input"
-              {...register('instructions', {
-                required: 'Instructions is required',
-              })}
-            ></textarea>
+            ></input>
+          </h3>
+          <h3>
+            Ingredients:
+            <section>
+              <button
+                className={`button ${theme}`}
+                type="button"
+                onClick={() => {
+                  append({ ingredient: '', measure: '' })
+                }}
+              >
+                Add new ingredient
+              </button>
+            </section>
+          </h3>
+          <ul>
+            {fields.map((item, index) => {
+              return (
+                <p key={item.id}>
+                  <input
+                    {...register(`ingredients.${index}.ingredient`, {
+                      required: true,
+                    })}
+                    placeholder="ingredient"
+                  />
+                  <Controller
+                    render={({ field }) => <input {...field} placeholder="measure" />}
+                    name={`ingredients.${index}.measure`}
+                    control={control}
+                  />
+                  <button className={`button ${theme}`} type="button" onClick={() => remove(index)}>
+                    Delete ingredient
+                  </button>
+                </p>
+              )
+            })}
+          </ul>
+          <h3>Instructions: </h3>
+          <textarea
+            className="instructions-input"
+            {...register('instructions', {
+              required: 'Instructions is required',
+            })}
+          ></textarea>
 
-            <h3>Info: </h3>
-            <p>
-              Category: <select
-                          {...register('category', {
-                            required: 'Category is required',
-                          })}
-                        >
-                          <option value="ordinary">Ordinary drink</option>
-                          <option value="cocktail">Cocktail</option>
-                          <option value="shot">Shot</option>
-                          <option value="punch">Punch / Party Drink</option>
-                        </select>
-            </p>
-            <p>
-              Glass: <select
-                        {...register('glass', {
-                          required: 'Glass is required',
-                        })}
-                      >
-                        <option value="cocktail-glass">Cocktail Glass</option>
-                        <option value="collins">Gollins Glass</option>
-                        <option value="shot-glass">Shot Glass</option>
-                        <option value="martini">Martini Glass</option>
-                        <option value="wine">Wine Glass</option>
-                        <option value="highball">Highball Glass</option>
-                        <option value="beer">Beer Mug</option>
-                        <option value="old-fashioned">Old-fashioned Glass</option>
-                      </select>
-            </p>
-            <p>
-              Alcoholic:
-              <label>
-                <input
-                  {...register('alcoholic')}
-                  type="radio"
-                  name="alcoholic"
-                  value={'yes'}
-                  checked
-                />
-                Yes
-              </label>
-              <label>
-                <input {...register('alcoholic')} type="radio" name="alcoholic" value="false" />
-                No
-              </label>
-            </p>
-            <h3>Picture: <input
-                            className='input'
-                            type="text"
-                            {...register('Picture', {
-                              required: 'Picture is required',
-                            })}
-                            placeholder='picture address'
-                          ></input>
-            </h3>
+          <h3>Info: </h3>
+          <p>
+            Category:{' '}
+            <select
+              {...register('category', {
+                required: 'Category is required',
+              })}
+            >
+              <option value="ordinary">Ordinary drink</option>
+              <option value="cocktail">Cocktail</option>
+              <option value="shot">Shot</option>
+              <option value="punch">Punch / Party Drink</option>
+            </select>
+          </p>
+          <p>
+            Glass:{' '}
+            <select
+              {...register('glass', {
+                required: 'Glass is required',
+              })}
+            >
+              <option value="cocktail-glass">Cocktail Glass</option>
+              <option value="collins">Gollins Glass</option>
+              <option value="shot-glass">Shot Glass</option>
+              <option value="martini">Martini Glass</option>
+              <option value="wine">Wine Glass</option>
+              <option value="highball">Highball Glass</option>
+              <option value="beer">Beer Mug</option>
+              <option value="old-fashioned">Old-fashioned Glass</option>
+            </select>
+          </p>
+          <p>
+            Alcoholic:
+            <label>
+              <input
+                {...register('alcoholic')}
+                type="radio"
+                name="alcoholic"
+                value={'yes'}
+                checked
+              />
+              Yes
+            </label>
+            <label>
+              <input {...register('alcoholic')} type="radio" name="alcoholic" value="false" />
+              No
+            </label>
+          </p>
+          <h3>
+            Picture:{' '}
+            <input
+              className="input"
+              type="text"
+              {...register('Picture', {
+                required: 'Picture is required',
+              })}
+              placeholder="picture address"
+            ></input>
+          </h3>
         </div>
-      <button className={`submit-button ${theme}`} type="submit">
-            Add drink
-          </button>
+        <button className={`submit-button ${theme}`} type="submit">
+          Add drink
+        </button>
       </form>
     </div>
   )
